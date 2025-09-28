f9f996ba4cc0fec2e334cb8932b6c09af4eb48ab
Thales Augusto <thallles.ramos@gmail.com>
2025-09-28 17:45:10 -0300

    Add Rack CORS middleware configuration for local development

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,12 +5,23 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
-#
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins "localhost:3000", "localhost:3001", "localhost:4200", "localhost:5173", "localhost:5174", "localhost:8080",
+            "127.0.0.1:3000", "127.0.0.1:3001", "127.0.0.1:4200", "127.0.0.1:5173", "127.0.0.1:5174", "127.0.0.1:8080"
+
+    resource "*",
+      headers: :any,
+      methods: [ :get, :post, :put, :patch, :delete, :options, :head ],
+      credentials: true
+  end
+
+  # allow do
+  #   origins "https://seu-frontend-domain.com"
+  #
+  #   resource "*",
+  #     headers: :any,
+  #     methods: [:get, :post, :put, :patch, :delete, :options, :head],
+  #     credentials: true
+  # end
+end
