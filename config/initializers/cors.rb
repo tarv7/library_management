14fd5161a111f41e909fbdb7324e69b09945f630
Thales Augusto <thallles.ramos@gmail.com>
2025-09-29 00:12:05 -0300

    Configure CORS to production env

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -16,12 +16,12 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       credentials: true
   end
 
-  # allow do
-  #   origins "https://seu-frontend-domain.com"
-  #
-  #   resource "*",
-  #     headers: :any,
-  #     methods: [:get, :post, :put, :patch, :delete, :options, :head],
-  #     credentials: true
-  # end
+  allow do
+    origins "library-management-flame-phi.vercel.app"
+
+    resource "*",
+      headers: :any,
+      methods: [ :get, :post, :put, :patch, :delete, :options, :head ],
+      credentials: true
+  end
 end
